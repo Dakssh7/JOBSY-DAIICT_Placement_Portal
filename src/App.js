5db0af0087639486265abea68633fbe95cc2681d
Daksh <dakshagravat1@gmail.com>
2023-01-03 19:31:39 +0530

    imporved login page UI

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Explorework from "./Components/Explorework";
 import Landingpage from "./Components/Landingpage";
-import Login from "./Components/Login";
 import Navbar from "./Components/Navbar";
 import Profiles from "./Components/Profiles";
+import Signin from './Components/Signin';
 import ViewProfile from "./Components/ViewProfile";
 
 function App() {
@@ -17,11 +17,10 @@ function App() {
       <AnimatePresence mode="wait" initial={false}>
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Landingpage />} />
-
           <Route path="/explorework" element={<Explorework />} />
           <Route path="/hiretalent" element={<Profiles />} />
           <Route path="/hiretalent/:userID" element={<ViewProfile />} />
-          <Route path="/getstarted" element={<Login />} />
+          <Route path="/getstarted" element={<Signin />} />
           <Route path="*" element={<h1> Not Found</h1>} />
         </Routes>
       </AnimatePresence>
